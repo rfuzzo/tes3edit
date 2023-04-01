d2542bc56667290f1837a0b79f97924ffd09d596
rfuzzo <r.fuzzo@gmail.com>
2023-04-01 10:54:34 +0200

    rename

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ fn main() -> eframe::Result<()> {
 
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
-        "eframe template",
+        "tes3edit",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(tes3edit::TemplateApp::new(cc))),
     )
 }
 
