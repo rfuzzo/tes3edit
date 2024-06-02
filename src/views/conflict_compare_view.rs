ef7c2cc26f4933604f8f3590a235691b9ce08d16
Moritz Baron <r.fuzzo@gmail.com>
2024-06-02 15:09:23 +0200

    update tes3

--- src/views/conflict_compare_view.rs
+++ src/views/conflict_compare_view.rs
@@ -1,6 +1,6 @@
 use crate::{get_unique_id, CompareData, TemplateApp, UiData};
 use egui::epaint::ahash::HashMap;
-use tes3::esp::editor::Editor;
+use tes3::esp::traits::Editor;
 
 impl TemplateApp {
     pub fn conflict_compare_view(&mut self, ui: &mut egui::Ui) {
